4f09761ad2d7cd079870bf5daba6d71fa23bd058
ParkSeokJin1 <skatksdml1@naver.com>
2023-09-13 17:38:00 +0900

    device 화면 추가

--- 15. 프로젝트/pages/device-choice.js
+++ 15. 프로젝트/pages/device-choice.js
@@ -1,5 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { useRouter } from "next/router"
+import BottomButton from "../src/components/BottomButton";
 
 export default function deviceChoice() {
-  return <div>deviceChoice</div>;
+  const router = useRouter();
+  const [selectedDevice, setSelectedDevice] = useState('')
+  
+  const onChangeHandler = (e) => {
+    setSelectedDevice(e.target.value)
+  }
+
+  console.log(selectedDevice)
+
+  
+
+  return (
+    <div className="container">
+      <div className="wrapper">
+        <h3 className="title">
+         아이맥과 맥북 중 원하는 장비를 선택해주세요.
+        </h3>
+    </div>
+    <label>
+      <input type="radio" name="device" id="diVice" value="iMac" onChange={onChangeHandler}/>
+      iMac
+    </label>
+    <label>
+      <input type="radio" name="device" id="diVice" value="MacBook" onChange={onChangeHandler}/>
+      MacBook
+    </label>
+
+
+
+    <BottomButton onClick={() => router.push('/accessory-choice') }>
+      악세서리 선택 화면으로 넘어가기
+      </BottomButton>
+      </div>
+     
+  );
 }
